5668f0699506b371d391127cac2f0454f10bb5bd
HarishMadavan <harishmadavan47@gmail.com>
2024-09-23 15:51:38 +0530

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,12 @@
+
+from flask import Flask
+
+#Welcome 
+app = Flask(_name_)
+
+@app.route('/')
+def welcome():
+    return 'Welcome to my web application!'
+
+if _name_ == '_main_':
+    app.run(debug=True)
